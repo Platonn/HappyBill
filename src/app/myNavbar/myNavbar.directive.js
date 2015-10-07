4ba85d8a3543a74f8239e925cd936c401c8a74c9
Platonn <platonn@platonn.com>
2015-10-07 20:34:23 +0200

    Moved navbar to directive myNavbar

---
+++ src/app/myNavbar/myNavbar.directive.js
@@ -0,0 +1,43 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('happyBill')
+        .directive('myNavbar', myNavbar);
+
+    /** @ngInject */
+    function myNavbar() {
+        var directive = {
+            restrict: 'E',
+            templateUrl: 'app/myNavbar/myNavbar.html',
+            scope: {
+                navbarCollapsed: '='
+            },
+            controller: NavbarController,
+            controllerAs: 'navbarCtrl', // 'vm', spike
+            bindToController: true
+        };
+
+        return directive;
+
+        /** @ngInject */
+        function NavbarController() {
+            var vm = this;
+            vm.navbarCollapsed; // "vm.navbarCollapsed" is avaible by directive option "bindToController: true"
+            vm.toggleNavbar = toggleNavbar;
+
+            activate();
+
+            ////////////////
+
+            function toggleNavbar() {
+                vm.navbarCollapsed = !vm.navbarCollapsed
+            }
+
+            function activate() {
+                vm.navbarCollapsed = true;
+            }    
+        }
+    }
+
+})();
